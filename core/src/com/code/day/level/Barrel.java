5cb3c7930722b9ffdfa1319417636763f4969d04
Aaron Cohen <aaroncohen73@gmail.com>
2015-11-07 14:23:57 -0800

    Level classes

---
+++ core/src/com/code/day/level/Barrel.java
@@ -0,0 +1,10 @@
+package com.code.day.level;
+
+/**
+ * Created by aaron on 11/7/15.
+ */
+public class Barrel{
+
+
+
+}
---
+++ core/src/com/code/day/level/Ladder.java
@@ -0,0 +1,7 @@
+package com.code.day.level;
+
+/**
+ * Created by aaron on 11/7/15.
+ */
+public class Ladder {
+}
