9e93bd0fc22062a11272b4d3582403a2528bde36
Merfoo <fauzimerfoo@gmail.com>
2015-11-07 15:51:29 -0800

    Added basic GameScreen class

---
+++ core/src/com/code/day/GameScreen.java
@@ -0,0 +1,44 @@
+package com.code.day;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by merfoo on 11/7/15.
+ */
+public class GameScreen implements Screen {
+
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void hide(){
+
+    }
+
+    @Override
+    public void pause(){
+
+    }
+
+    @Override
+    public void resume(){
+
+    }
+
+    @Override
+    public void resize(int width, int height){
+
+    }
+
+    @Override
+    public void render(float delta){
+
+    }
+
+    @Override
+    public void dispose(){
+
+    }
+}
