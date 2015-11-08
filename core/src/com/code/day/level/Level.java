5618071630c86b59ff6f55b3d96331cddcc4b0cb
Duncan Parsons <brownpad@oregonstate.edu>
2015-11-07 22:26:54 -0800

    merge master

--- core/src/com/code/day/level/Level.java
+++ core/src/com/code/day/level/Level.java
@@ -16,17 +16,19 @@ public class Level {
     public void load() {
         girders = new ArrayList<Girder>();
         barrels = new ArrayList<Barrel>();
-        
-        girders.add(Girder.createGirder (15, 160, 126, 160, true));//girder1 flat
-        girders.add(Girder.createGirder (135, 159, 200, 154, true));//girder1
-        girders.add(Girder.createGirder(31, 126, 223, 135, true));//girder2
-        girders.add(Girder.createGirder(15, 104, 200, 96, true));//girder3
-        girders.add(Girder.createGirder(31, 66, 223, 75, true));//girder4
-        girders.add(Girder.createGirder(15, 44, 200, 36, true));//girder5
-        girders.add(Girder.createGirder(7, 8, 130, 8, true));//girder6 flat
-        girders.add(Girder.createGirder(150, 10, 223, 14, true));//girder6
-        girders.add(Girder.createGirder(79, 177, 100, 177, true));//The_unnamed_woman lower
-        girders.add(Girder.createGirder(103, 185, 150, 185, true));//peach higher
+
+        girders.add(Girder.createGirder (15, 160, 126, 160, true));//girder1 flat 0
+        girders.add(Girder.createGirder (135, 159, 200, 154, true));//girder1 1
+        girders.add(Girder.createGirder(31, 126, 223, 135, true));//girder2 2
+        girders.add(Girder.createGirder(15, 104, 200, 96, true));//girder3 3
+        girders.add(Girder.createGirder(31, 66, 223, 75, true));//girder4 4
+        girders.add(Girder.createGirder(15, 44, 200, 36, true));//girder5 5
+        girders.add(Girder.createGirder(7, 8, 130, 8, true));//girder6 flat 6
+        girders.add(Girder.createGirder(150, 10, 223, 14, true));//girder6 7
+        girders.add(Girder.createGirder(79, 177, 100, 177, true));//The_unnamed_woman lower 8
+        girders.add(Girder.createGirder(103, 185, 150, 185, true));//The_unnamed_woman higher 9
+        Ladder.createLadder(104, girders.get(0), girders.get(2), true);
+
     }
 
     public void update(float delta) {
